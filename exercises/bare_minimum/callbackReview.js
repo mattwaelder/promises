39830fa59aafd2c8cd16798a8f3206f4a34fc4df
Matt Waelder <mattwaelder@gmail.com>
2022-08-12 00:36:55 -0700

    complete up to statuscode

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -5,14 +5,26 @@
 var fs = require('fs');
 var request = require('needle');
 
+
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
-};
+var pluckFirstLineFromFile = (filepath, callback) => {
+  fs.readFile(filepath, (err, data) => {
+    if (err) {
+      return callback(err)
+    }
+    callback(null, data.toString().split('\n')[0])
+  })
+}
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = (url, callback) => {
+  request.get(url, (err, data) => {
+    if (err) {
+      callback(err)
+    } else {
+      callback(null, data.statusCode)
+    }
+  })
 };
 
 // Export these functions so we can test them and reuse them in later exercises
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -8,9 +8,22 @@ var fs = require('fs');
 var request = require('needle');
 var Promise = require('bluebird');
 
+// var readFile = Promise.promisify(fs.readFile);
+
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+var pluckFirstLineFromFileAsync = (filePath) => {
+  return new Promise ((resolve, reject) => {
+    fs.readFile(filePath, (err, data) => {
+      if(err) {
+        return reject(err);
+      }
+      resolve(data)
+    })
+  }).then((data) => {
+    return data.toString().split('\n')[0];
+  })
+
+  pluckFirstLineFromFileAsync.catch((err) => console.log(err))
 };
 
 // This function should retrieve the status code of a GET request to `url`
